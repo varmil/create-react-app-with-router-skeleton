6cc026f81238769cb1e5151d1f073e44e2029039
Akihiro Yamamoto(home-mac) <ay@ay.local>
2016-08-27 23:25:28 +0900

    refactor src/pages/Base.js

--- src/pages/Base.js
+++ src/pages/Base.js
@@ -1,19 +1,19 @@
 import { Component } from 'react';
 // import { Container } from 'flux/utils';
-// import PageNowLoadingStore from '../stores/PageNowLoadingStore';
+// import LoadingStore from '../stores/LoadingStore';
 
 // TODO: Implement Container
 
 class Base extends Component {
 	// static getStores(): Array<Store> {
-	// 	return [PageNowLoadingStore];
+	// 	return [LoadingStore];
 	// }
 	//
 	// static calculateState(prevState: ?State): State {
 	// 	console.info('[Base][calculateState] prevState', prevState);
 	//
 	// 	return {
-	// 		nowLoading: PageNowLoadingStore.getState(),
+	// 		nowLoading: LoadingStore.getState(),
 	// 	};
 	// }
 
